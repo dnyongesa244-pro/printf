eda318ca433695403a6520e203f74518c9e558bd
dnyongesa244-pro <dnyongesa244@gmail.com>
2023-07-24 04:36:57 +0300

    created function to print integer

--- _printf.c
+++ _printf.c
@@ -1,101 +1,120 @@
 #include <stdio.h>
-#include <stdarg.h>
 #include <string.h>
-#include "main.h"
+#include <stdarg.h>
 void printint(int num)
 {
-	int i;
+	int i, j;
 	int temp;
+	
 	int size;
-	int j;
-	int *array;
 	if(num < 0)
 	{
 		putchar('-');
 		num *= -1;
 	}
-	if(num == '0')
-		putchar('0');
-	temp = num;
-	size = 0;
-	while(num != 0)
+	if(num == 0)
 	{
-		num/=10;
+		putchar('0');
+	}
+	size = 0;
+	temp = num;
+	while(temp != 0 )
+	{
+		temp /= 10;
 		size++;
 	}
-	array[size];
-
-	for(i = 0 ; i < size ; i ++)
+	i = 0;
+	int array[size];
+	while(num != 0)	
 	{
-		array[i] = num%10;
+		array[i] = num % 10;
 		num /= 10;
+		i++;
 	}
-
-	for(j = 0 ; j < size ; j++)
+	for(i = size-1; i>=0 ; i--)
 	{
-		putchar('0' + array[j]);
+		putchar('0' + array[i]);
 	}
+	putchar('\n');
 }
 void printstring(char *word)
 {
-	int i;
 	int x;
 	x = strlen(word);
-	i = 0;
-	while(i < x)
+	
+	int i;
+	
+	while(i<x)
 	{
 		putchar(word[i]);
 		i++;
 	}
+	putchar('\n');
 }
 void print(const char *format, ...)
 {
-	va_list args;
-	va_start(args, format);
+        va_list args;
+        va_start(args, format);
 
-	while(*format)
-	{
-		if(*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'i':
-					{
-						int value = va_arg(args, int);
-						printint(value);
-						break;
-					}
-				case 'c':
-					{
-						int value = va_arg(args, int);
-						printf("%c", value);
-						break;
-					}
-				case 'f':
-					{
-						double value = va_arg(args, double);
-						printf("%f", value);
-						break;
-					}
-				case 's':
-					{
-						char *value = va_arg(args, char*);
-						printf("%s", value);
-						break;
-					}
-				default:
-					{
-						putchar(*format);
-						break;
-					}
-			}
-		}
-		else
-		{
-			putchar(*format);
-		}
-		format++;
-	}
-	va_end(args);
+        while(*format)
+        {
+                if(*format == '%')
+                {
+                        format++;
+                        switch (*format)
+                        {
+                                case 'd':
+                                        {
+                                                int value = va_arg(args, int);
+                                                printint(value);
+                                                
+                                                break;
+                                        }
+                                case 'c':
+                                        {
+                                                int value = va_arg(args, int);
+                                                printf("%c", value);
+                                                break;
+                                        }
+                                case 'f':
+                                        {
+                                                double value = va_arg(args, double);
+                                                printf("%f", value);
+                                                break;
+                                        }
+                                case 's':
+                                        {
+                                                char *value = va_arg(args, char*);
+                                                printstring(value);
+                                                break;
+                                        }
+                                default:
+                                        {
+                                                putchar(*format);
+                                                break;
+                                        }
+                        }
+                }
+                else
+                {
+                        putchar(*format);
+                }
+                format++;
+        }
+        va_end(args);
+}
+int main(void)
+{
+	int x;
+	char *name;
+	char ch = 'd';
+	float point;
+	
+    name = "daniel";
+    
+    print("%s",name);
+	x = 309;
+
+	
+	print("%d",x);
 }
