2cc80beabf8f6e861456cb63b6acacebcc62b819
dnyongesa244-pro <dnyongesa244@gmail.com>
2023-07-25 23:40:18 +0300

    betty documentation added

--- get_flags.c
+++ get_flags.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
+ * get_flags -  function to Calculates active flags
+ *
  * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
+ *
+ * @i: take a parameter 
+ *
+ * Return: Flags (success)
  */
 int get_flags(const char *format, int *i)
 {
