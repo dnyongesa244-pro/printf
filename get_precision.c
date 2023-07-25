182ca11e00d4908f38aa4bf6407bc3c060ff7f3b
dnyongesa244-pro <dnyongesa244@gmail.com>
2023-07-25 23:55:16 +0300

    betty documentation

--- get_precision.c
+++ get_precision.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
+ * get_precision - function to Calculate the precision for printing
+ *
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ *
+ * @i: List of arguments to be printed parameter
+ *
  * @list: list of arguments.
  *
- * Return: Precision.
+ * Return: Precision (Suceess)
  */
 int get_precision(const char *format, int *i, va_list list)
 {
